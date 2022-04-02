8d59aea7835b0341bdd6c58678258b4efbaff6ac
weekbelt <vfrvfr4207@gmail.com>
2022-04-02 18:07:26 +0900

    2) 정적 자원 관리 - WebIgnore 설정

--- src/main/java/me/weekbelt/corespringsecurity/configs/SecurityConfig.java
+++ src/main/java/me/weekbelt/corespringsecurity/configs/SecurityConfig.java
@@ -1,10 +1,12 @@
 package me.weekbelt.corespringsecurity.configs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,8 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         String password = passwordEncoder().encode("1111");
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("USER","MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("USER","MANAGER","ADMIN");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("USER", "MANAGER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("USER", "MANAGER", "ADMIN");
     }
 
     @Bean
@@ -29,16 +31,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/mypage").hasRole("USER")
-                .antMatchers("/messages").hasRole("MANAGER")
-                .antMatchers("/config").hasRole("ADMIN")
-                .anyRequest().authenticated()
+            .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/mypage").hasRole("USER")
+            .antMatchers("/messages").hasRole("MANAGER")
+            .antMatchers("/config").hasRole("ADMIN")
+            .anyRequest().authenticated()
 
-        .and()
-                .formLogin();
+            .and()
+            .formLogin();
     }
 }
