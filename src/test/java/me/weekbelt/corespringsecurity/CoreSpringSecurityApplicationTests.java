6f1b4f295d7b48f577a56347136fbd586512e440
weekbelt <vfrvfr4207@gmail.com>
2022-04-02 17:58:02 +0900

    1) 실전 프로젝트 생성

---
+++ src/test/java/me/weekbelt/corespringsecurity/CoreSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.corespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
